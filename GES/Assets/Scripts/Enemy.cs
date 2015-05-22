6ea3d246a0985e304960241ef18d2fc867a4ce34
Kero Soh <keroame@hotmail.com>
2015-05-22 16:05:23 +0800

    Spawner.cs

---
+++ GES/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;      // movement speed of unit in z axis
+
+	void Start () {
+        GetComponent<Animator>().SetFloat("Speed", 1.0f);   // Speed requires at least a moveSpeed of 0.5 to trigger move animation
+	}
+
+    void Update()
+    {
+        // Linearly interpolate its position in the z axis in accordance to moveSpeed
+        transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(0, 0, 1), moveSpeed * Time.deltaTime);
+    }
+}
---
+++ GES/Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject spawnUnit;        // Prefab to spawn
+    public float spawnDelay = 2;        // Interval between spawn times (seconds)
+
+    void Start()
+    {
+        StartCoroutine(SpawnUnits(spawnDelay));     // Starts the SpawnUnits coroutine
+    }
+
+    IEnumerator SpawnUnits(float _spawnDelay)
+    {
+        while (true)        // run forever
+        {
+            // Spawn the prefab at spawner position with default rotation values
+            Instantiate(spawnUnit, transform.position, Quaternion.identity);
+
+            // Wait for spawnDelay seconds before resuming the WHILE loop
+            yield return new WaitForSeconds(_spawnDelay); 
+        }   
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draws a red cube only in the editor to display spawner location
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position + new Vector3(0, 0.5f, 0), Vector3.one);
+    }
+}
