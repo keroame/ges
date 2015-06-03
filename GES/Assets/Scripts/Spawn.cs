b315daf059e7d055877b8d920ce0defcf6daeb38
Kero Soh <keroame@hotmail.com>
2015-06-03 14:50:25 +0800

    Spawner.cs update
    • Spawning must stop/ pause when it is in view of player’s FOV.
    • Spawners must have an adjustable max limit amount of units it can
    spawn.

--- GES/Assets/Scripts/Spawn.cs
+++ GES/Assets/Scripts/Spawn.cs
@@ -3,26 +3,68 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
 
-    public GameObject spawnUnit;        // Prefab to spawn
-    public bool oneTimeSpawn = false;   // If spawner will only spawn once
-    public float spawnDelay = 2;        // Interval between spawn times (seconds)
-
-    void Start()
+    public enum SpawnerType
     {
-        if (!oneTimeSpawn)          // If spawner is not a one-time spawner
+        Player,
+        Enemy
+    }
+
+    public SpawnerType spawnerType = SpawnerType.Enemy;     // Type of spawner
+    public GameObject spawnUnit;                            // Prefab to spawn
+    public float maxSpawns = 1;                             // Maximum number of spawns
+    public float spawnDelay = 2;                            // Interval between spawn times (seconds)
+
+    private float spawnCount = 0;                           // Number of units already spawned
+
+    void Awake()                                            // Player spawns before enemies
+    {
+        if (spawnerType == SpawnerType.Player)              // if spawner is spawning player
         {
-            InvokeRepeating("SpawnUnit", 0, spawnDelay);     // Calls SpawnUnit() method, repeats every spawnDelay seconds
-        }
-        else
-        {
-            SpawnUnit();
+            StartCoroutine(SpawnUnit());                    // spawn player
         }
     }
 
-    void SpawnUnit()
+    void Start()                                            // enemy spawns after player
     {
-        // Spawn the prefab at spawner position with spawner's rotation values
-        Instantiate(spawnUnit, transform.position, transform.rotation);  
+        if (spawnerType == SpawnerType.Enemy)               // if spawner is spawning enemy
+        {
+            StartCoroutine(SpawnUnit());                    // spawn enemy
+        }
+    }
+
+    IEnumerator SpawnUnit()
+    {
+        while (spawnCount < maxSpawns)                      // check if already reached max no. of spawns
+        {
+            if (spawnerType == SpawnerType.Enemy)           // if player is enemy
+            {
+                if (Camera.main != null)                    // check if there is a main camera
+                {
+                    if (!InCameraView())                    // if spawner is outside camera view
+                    {
+                        // Spawn the prefab at spawner position with spawner's rotation values
+                        Instantiate(spawnUnit, transform.position, transform.rotation);
+                        spawnCount++;                        // increment number of spawned units
+                        yield return new WaitForSeconds(spawnDelay);    // wait for spawnDelay seconds before continuing spawn function
+                    }
+                }
+            }
+            else
+            {
+                // Spawn the prefab at spawner position with spawner's rotation values
+                Instantiate(spawnUnit, transform.position, transform.rotation);
+                spawnCount++;       // increment number of spawned units
+            }
+        }    
+    }
+
+    private bool InCameraView()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);           // return main camera's six planes
+        if (GeometryUtility.TestPlanesAABB(planes, GetComponent<Collider>().bounds))    // check if spawner is within the bounds of the six planes
+            return true;
+        else
+            return false;
     }
 
     void OnDrawGizmos()
